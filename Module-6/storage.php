653702057dc5c089d0738fd50767645f199f9530
Denis <kanukov.denis@gmail.com>
2022-09-16 12:06:11 +0300

    add editPost function

--- Module-6/storage.php
+++ Module-6/storage.php
@@ -1,23 +1,38 @@
 <?php
-    $textStorage = [
-    ];
+    $textStorage = [];
 
     function addPost(string $title, string $text, &$textStorage): void {
         $textStorage[] = ['title' => $title, 'text' => $text];
     }
 
     function removePost(int $index, array &$textStorage): bool {
-        if (count($textStorage) - 1 < $index) return false;
+        if (!array_key_exists($index, $textStorage)) return false;
 
         unset($textStorage[$index]);
+        return true;
+    }
+
+    function editPost(int $index, string $title, string $text, array &$textStorage): bool {
+        if (!array_key_exists($index, $textStorage)) return false;
+
+        $textStorage[$index]['title'] = $title;
+        $textStorage[$index]['text'] = $text;
         return true;
     }
 
     addPost('testTitle3', 'testText3', $textStorage);
     addPost('testTitle4', 'testText4', $textStorage);
     print_r($textStorage);
+
     $res1 = removePost(0, $textStorage);
     $res2 = removePost(3, $textStorage);
     print_r($textStorage);
-    echo $res1 . " " . $res2, PHP_EOL;
-//    var_dump($testStorage[1]['title']);
+    var_dump($res1, $res2);
+
+    $res = editPost(1, 'newTitle', 'newText', $textStorage);
+    $res2 = editPost(10, 'newTitle10', 'newText10', $textStorage);
+    print_r($textStorage);
+    var_dump($res, $res2);
+
+//    var_dump($textStorage[1]['title']);
+
