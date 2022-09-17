fb7ac2789c1774ec4e8886ef0eb48f6679d91d89
Denis <kanukov.denis@gmail.com>
2022-09-17 17:56:25 +0300

    upd to PSR12 coding style

--- Module-6/storage.php
+++ Module-6/storage.php
@@ -1,39 +1,42 @@
 <?php
-    $textStorage = [];
+$textStorage = [];
 
-    function addPost(string $title, string $text, &$textStorage): void
-    {
-        $textStorage[] = ['title' => $title, 'text' => $text];
+function addPost(string $title, string $text, &$textStorage): void
+{
+    $textStorage[] = ['title' => $title, 'text' => $text];
+}
+
+function removePost(int $index, array &$textStorage): bool
+{
+    if (!array_key_exists($index, $textStorage)) return false;
+
+    unset($textStorage[$index]);
+    return true;
+}
+
+function editPost(int $index, string $title, string $text, array &$textStorage): bool
+{
+    if (!array_key_exists($index, $textStorage)) {
+        return false;
     }
 
-    function removePost(int $index, array &$textStorage): bool {
-        if (!array_key_exists($index, $textStorage)) return false;
+    $textStorage[$index]['title'] = $title;
+    $textStorage[$index]['text'] = $text;
+    return true;
+}
 
-        unset($textStorage[$index]);
-        return true;
-    }
+addPost('testTitle3', 'testText3', $textStorage);
+addPost('testTitle4', 'testText4', $textStorage);
+print_r($textStorage);
 
-    function editPost(int $index, string $title, string $text, array &$textStorage): bool {
-        if (!array_key_exists($index, $textStorage)) return false;
+$res1 = removePost(0, $textStorage);
+$res2 = removePost(3, $textStorage);
+print_r($textStorage);
+var_dump($res1, $res2);
 
-        $textStorage[$index]['title'] = $title;
-        $textStorage[$index]['text'] = $text;
-        return true;
-    }
+$res = editPost(1, 'newTitle', 'newText', $textStorage);
+$res2 = editPost(10, 'newTitle10', 'newText10', $textStorage);
+print_r($textStorage);
+var_dump($res, $res2);
 
-    addPost('testTitle3', 'testText3', $textStorage);
-    addPost('testTitle4', 'testText4', $textStorage);
-    print_r($textStorage);
-
-    $res1 = removePost(0, $textStorage);
-    $res2 = removePost(3, $textStorage);
-    print_r($textStorage);
-    var_dump($res1, $res2);
-
-    $res = editPost(1, 'newTitle', 'newText', $textStorage);
-    $res2 = editPost(10, 'newTitle10', 'newText10', $textStorage);
-    print_r($textStorage);
-    var_dump($res, $res2);
-
-//    var_dump($textStorage[1]['title']);
 
